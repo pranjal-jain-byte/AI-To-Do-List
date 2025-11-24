22ce4afe5e1128074f94c4c04a8bffecffedeb1e
PRANJAL JAIN <23ucc582@lnmiit.ac.in>
2025-11-24 07:19:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1952245850-2402e",
+  "appId": "1:222921629478:web:0d2a707742b77e090e6be7",
+  "apiKey": "AIzaSyDzq4eCHbCSOwa5v3i_AuCTQfYmvCq8lWc",
+  "authDomain": "studio-1952245850-2402e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "222921629478"
+};
